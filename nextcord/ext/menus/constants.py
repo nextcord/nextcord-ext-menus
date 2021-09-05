bf108fec03efce44aaabd183a38cfde60b498594
Jonah Lawrence <jonah@freshidea.com>
2021-09-05 03:53:29 +0300

    refactoring into multiple files

---
+++ nextcord/ext/menus/constants.py
@@ -0,0 +1,8 @@
+import logging
+
+
+# consistency with the `nextcord` namespaced logging
+log = logging.getLogger(__name__)
+
+# default timeout parameter for menus in seconds
+DEFAULT_TIMEOUT = 180.0
