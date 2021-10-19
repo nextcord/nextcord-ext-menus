4633b379ce583ab92abc103d2c39f0704b629662
Jonah Lawrence <jonah@freshidea.com>
2021-10-19 14:16:34 +0300

    Update custom buttons example

--- examples/custom_buttons.py
+++ examples/custom_buttons.py
@@ -13,20 +13,33 @@ class CustomButtonMenuPages(menus.ButtonMenuPages, inherit_buttons=False):
     This allows us to add our own buttons with custom labels, colors, or even callbacks. 
     """
 
+    # You can override the emojis used for the buttons by adding them to the class.
+    # Both default emojis and custom emojis can be specified as shown below.
+    FIRST_PAGE = "<:pagefirst:899973860772962344>"
+    PREVIOUS_PAGE = "\N{WHITE LEFT POINTING BACKHAND INDEX}"
+    NEXT_PAGE = "\N{WHITE RIGHT POINTING BACKHAND INDEX}"
+    LAST_PAGE = "<:pagelast:899973860810694686>"
+    STOP = "\N{BLACK SQUARE FOR STOP}"
+
+
     def __init__(self, source, timeout=60):
         super().__init__(source, timeout=timeout)
-        # Note: for the default button callback to work, the emoji must be the same.
-        # None of the buttons are required; you can leave any of them out.
+        
+        # You can change the buttons to have custom labels and styles by setting
+        # inherit_buttons=False above and adding them with self.add_item as shown below.
+        # Note: None of the buttons are required you can leave any of them out
         self.add_item(menus.MenuPaginationButton(emoji=self.FIRST_PAGE, label="First"))
         self.add_item(menus.MenuPaginationButton(emoji=self.PREVIOUS_PAGE, label="Prev"))
         self.add_item(menus.MenuPaginationButton(emoji=self.NEXT_PAGE, label="Next"))
         self.add_item(menus.MenuPaginationButton(emoji=self.LAST_PAGE, label="Last"))
-        # place the Stop button (first button) at the end of the list
+        
+        # Rearrange the buttons (place the Stop button (first button) at the end of the list)
         self.children = self.children[1:] + self.children[:1]
-        # disable buttons that are unavailable to be pressed
+        
+        # Disable buttons that are unavailable to be pressed at the start
         self._disable_unavailable_buttons()
 
-    # To change the emoji or callback function, we can use the UI button decorator
+    # To change the callback function, we can use the button decorator
     @nextcord.ui.button(emoji="\N{CROSS MARK}", label="Stop")
     async def stop_button(self, button, interaction):
         await interaction.response.send_message("You pressed stop.", ephemeral=True)
